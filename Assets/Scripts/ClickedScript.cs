f9a49a8ce0298f204fafaf1d9197dde24af8a395
DESKTOP-SEMRO1M\Auzergos <Auzergos@gmail.com>
2018-07-30 16:23:15 +0600

    Начало проекта

---
+++ Assets/Scripts/ClickedScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickedScript : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+
+    public bool clickedIs = false;
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        clickedIs = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        clickedIs = false;
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public ClickedScript[] MoveRope;
+    public GameObject Rope;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate(){
+        if (MoveRope[0].clickedIs == true){
+            Rope.transform.position=new Vector2(Rope.transform.position.x-0.1f, Rope.transform.position.y);
+        }
+        if (MoveRope[1].clickedIs == true)
+        {
+            Rope.transform.position = new Vector2(Rope.transform.position.x + 0.1f, Rope.transform.position.y);
+        }
+    }
+}
