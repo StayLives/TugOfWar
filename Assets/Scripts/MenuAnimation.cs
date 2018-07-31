71b41a169ebfd425bf7dbd12a15be38ce6fda67a
DESKTOP-SEMRO1M\Auzergos <Auzergos@gmail.com>
2018-07-31 17:55:13 +0600

    Стартовое меню

---
+++ Assets/Scripts/MenuAnimation.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuAnimation : MonoBehaviour
+{
+    
+    public float distance;
+    public Vector3 direction;
+    public float speed;
+    private Vector3 startPosition;
+    bool back = false;
+    // Use this for initialization
+    void Start () {
+        startPosition = transform.position;
+        
+    }
+
+    public void GoPlay(){
+        SceneManager.LoadScene("TugOfWar");
+        
+    }
+	
+	void FixedUpdate (){
+        if (Vector3.Distance(transform.position, startPosition) < distance && !back)
+        {
+            transform.position += direction.normalized * 0.5f * Time.deltaTime;
+        }
+        else
+            back = true;
+
+        if (back)
+        {
+            float tmp = Vector3.Distance(transform.position, startPosition);
+
+            transform.position -= direction.normalized * 0.5f * Time.deltaTime;
+
+            if (Vector3.Distance(transform.position, startPosition) > tmp)
+            {
+                back = false;
+            }
+        }
+        //if (transform.position.x<=2){
+        //        transform.position = Vector3.MoveTowards(transform.position, transform.position + direct, 0.5f * Time.deltaTime);
+        //    }
+
+
+    }
+}
