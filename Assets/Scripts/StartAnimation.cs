d80e698fe1b49e3bb513ba9e33d56b2cc52591f9
Auzergos <Auzergos@gmail.com>
2018-07-31 21:42:37 +0600

    Таймер

---
+++ Assets/Scripts/StartAnimation.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAnimation : MonoBehaviour {
+    public GameObject[] obj;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(StartGame());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    IEnumerator StartGame()
+    {
+        foreach (var item in obj)
+        {
+
+        var cloneBomb=Instantiate(item, item.transform.position, Quaternion.identity);
+            Destroy(cloneBomb, 1);  
+        yield return new WaitForSeconds(1);          
+        }
+       
+    }
+}
